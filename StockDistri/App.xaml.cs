df623443cc8b8d44e9251d9d9376275c9641b1bf
MONKKIII <gildwen45@gmail.com>
2024-01-26 10:23:46 +0100

    Update avec UserLogIn

--- StockDistri/App.xaml.cs
+++ StockDistri/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockDistri.MVVM.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,26 @@ namespace StockDistri
     /// </summary>
     public partial class App : Application
     {
+        // This method is called when the application starts
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Call the base class method first
+            base.OnStartup(e);
+
+            // Create an instance of the MainWindow
+            MainWindow mainWindow = new MainWindow();
+
+            // Disable interactions with the MainWindow initially
+            mainWindow.IsEnabled = false;
+
+            // Create an instance of the UserLogIn window
+            UserLogIn loginWindow = new UserLogIn();
+
+            // Show the UserLogIn window
+            loginWindow.Show();
+
+            // Bring the UserLogIn window to the front
+            loginWindow.Activate();
+        }
     }
 }
---
+++ StockDistri/MVVM/View/UserLogIn.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+namespace StockDistri.MVVM.View
+{
+    /// <summary>
+    /// Logique d'interaction pour UserLogIn.xaml
+    /// </summary>
+    public partial class UserLogIn : Window
+    {
+        public UserLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void CloseButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ValidateLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            // Add any validation or logic here if needed
+
+            // Close the UserLogIn window
+            this.Close();
+        }
+    }
+}
